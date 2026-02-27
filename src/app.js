b05f890693f59d3a29fb844d59a589dad6e6b3c0
lucie <kiyita.ajx@gmail.com>
2026-02-27 22:29:39 +0100

    ajout pages ejs + maj app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import helmet from 'helmet';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.routes.js';
 import noteRoutes from './routes/note.routes.js';
 
 /** Démarrage de l'application Express */
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware de sécurité avec Helmet
 app.use(helmet({
@@ -19,6 +26,7 @@ app.use(helmet({
 
 // Middleware pour parser le JSON avec une limite de taille
 app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //test si API fonctionne
@@ -28,6 +36,14 @@ app.get("/", (req, res) => {
 
 app.get('/tester', (req, res) => {
   res.redirect('/tester.html');
+});
+
+app.get('/connexion', (req, res) => {
+  res.render('connexion');
+});
+
+app.get('/notes', (req, res) => {
+  res.render('notes');
 });
 
 app.use('/api/auth', authRoutes);
